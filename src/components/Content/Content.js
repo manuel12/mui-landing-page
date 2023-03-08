9d531ea49adb81c6568fea088858a3a906b77263
Manuel <manuelpinedacabeza@gmail.com>
2023-03-08 15:55:11 +0100

    Added zoom and slide transitions.

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -7,6 +7,9 @@ import FacebookIcon from "@mui/icons-material/Facebook";
 import InstagramIcon from "@mui/icons-material/Instagram";
 import TwitterIcon from "@mui/icons-material/Twitter";
 
+import Slide from "@mui/material/Slide";
+import Zoom from "@mui/material/Zoom";
+
 import "./styles.css";
 
 const Content = () => {
@@ -15,42 +18,52 @@ const Content = () => {
       <Box className="content">
         <Box className="left-content">
           <Container>
-            <Box className="text-container">
-              <Typography variant="h3" align="left" gutterBottom>
-                Welcome to
-              </Typography>
-              <Typography variant="h1" align="center" gutterBottom>
-                Mario's Pizzeria
-              </Typography>
-              <Typography
-                className="text-location"
-                variant="h4"
-                align="center"
-                gutterBottom
-              >
-                ​254 W 27ST ST, NEW YORK, NY 10011
-              </Typography>
-            </Box>
-            <Box className="action-buttons-container">
-              <Button className="action-button">reservation</Button>
-              <Button className="action-button">view menu</Button>
-            </Box>
-            <Box className="social-buttons-container">
-              <Button className="social-button">
-                <FacebookIcon className="social-icon" />
-              </Button>
-              <Button className="social-button">
-                <InstagramIcon className="social-icon" />
-              </Button>
-              <Button className="social-button">
-                <TwitterIcon className="social-icon" />
-              </Button>
-            </Box>
+            <Zoom in={true} timeout={1000}>
+              <Box className="text-container">
+                <Typography variant="h5" align="center" gutterBottom>
+                  Welcome to
+                </Typography>
+                <Typography variant="h1" align="center" gutterBottom>
+                  Mario's Pizzeria
+                </Typography>
+                <Typography
+                  className="text-location"
+                  variant="h4"
+                  align="center"
+                  gutterBottom
+                >
+                  ​254 W 27ST ST, NEW YORK, NY 10011
+                </Typography>
+              </Box>
+            </Zoom>
+
+            <Slide direction="right" in={true} timeout={1000}>
+              <Box className="action-buttons-container">
+                <Button className="action-button">reservation</Button>
+                <Button className="action-button">view menu</Button>
+              </Box>
+            </Slide>
+
+            <Slide direction="left" in={true} timeout={1000}>
+              <Box className="social-buttons-container">
+                <Button className="social-button">
+                  <FacebookIcon className="social-icon" />
+                </Button>
+                <Button className="social-button">
+                  <InstagramIcon className="social-icon" />
+                </Button>
+                <Button className="social-button">
+                  <TwitterIcon className="social-icon" />
+                </Button>
+              </Box>
+            </Slide>
           </Container>
         </Box>
         <Box className="right-content">
           <Container>
-            <Box className="image-container"></Box>
+            <Zoom in={true} timeout={1500}>
+              <Box className="image-container"></Box>
+            </Zoom>
           </Container>
         </Box>
       </Box>
