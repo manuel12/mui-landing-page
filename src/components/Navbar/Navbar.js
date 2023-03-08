ea85113fac3bf33afa459dc9f4f22789f52eb3de
Manuel <manuelpinedacabeza@gmail.com>
2023-03-08 15:57:15 +0100

    Added function to handle navbar menu opening and closing. Added navbar menu button and hamburger menu.

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,29 +1,89 @@
-import React from "react";
+import React, { useState } from "react";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import Container from "@mui/material/Container";
 import Menu from "@mui/material/Menu";
 import MenuItem from "@mui/material/MenuItem";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 
 import "./styles.css";
 
 const Navbar = () => {
-  const pages = ["pizza", "pasta", "about", "location"];
+  const pages = ["home", "pizza", "pasta", "about", "location"];
+
+  const [anchorElNav, setAnchorElNav] = useState(null);
+  const [anchorElUser, setAnchorElUser] = useState(null);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
 
   return (
     <AppBar className="navbar">
       <Container maxWidth="xl">
         <Toolbar className="toolbar" disableGutters>
-          {pages.map((page) => {
-            return (
-              <Button className="navbar-button" variant="outlined">
-                {page}
-              </Button>
-            );
-          })}
+          <Box
+            sx={{
+              display: {
+                xs: "flex",
+                md: "none",
+              },
+            }}
+          >
+            <IconButton size="large" onClick={handleOpenNavMenu}>
+              <MenuIcon className="hamburger-button" />
+            </IconButton>
+            <Menu
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "left",
+              }}
+              keepMounted
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: "block", md: "none" },
+              }}
+            >
+              {pages.map((page) => (
+                <MenuItem
+                  key={page}
+                  onClick={handleCloseNavMenu}
+                  className="navbar-menu-item"
+                >
+                  <Typography textAlign="center" variant="h4">
+                    {page}
+                  </Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+          <Box
+            className="navbar-buttons-container"
+            sx={{
+              display: {
+                xs: "none",
+                md: "inherit",
+              },
+            }}
+          >
+            {pages.map((page) => {
+              return (
+                <Button className="navbar-button" variant="outlined">
+                  {page}
+                </Button>
+              );
+            })}
+          </Box>
         </Toolbar>
       </Container>
     </AppBar>
