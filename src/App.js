be786cc6cab52e02d9c7ff0bdceb3cff6f40dd24
Manuel <manuelpinedacabeza@gmail.com>
2023-03-10 16:28:46 +0100

    Added PizzaContent

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import Content from './components/Content/Content';
+import PizzaContent from './components/Content/PizzaContent';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
       <Content />
+      <PizzaContent />
     </div>
   );
 }
