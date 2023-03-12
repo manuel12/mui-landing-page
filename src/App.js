10a825602c0b0cafd454fca68d428ffee7a7e65c
Manuel <manuelpinedacabeza@gmail.com>
2023-03-12 20:30:12 +0100

    Added about section

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import './App.css';
-import Navbar from './components/Navbar/Navbar';
-import Content from './components/Content/Content';
-import PizzaContent from './components/Content/PizzaContent';
-import PastaContent from './components/Content/PastaContent';
+import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
+import Content from "./components/Content/Content";
+import PizzaContent from "./components/Content/PizzaContent";
+import PastaContent from "./components/Content/PastaContent";
+import About from "./components/Content/About";
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
+      <Navbar />
       <Content />
       <PizzaContent />
       <PastaContent />
+      <About />
     </div>
   );
 }
