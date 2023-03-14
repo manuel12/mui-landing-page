d9b2c9a44cddbb57ccc7bcc3aeb9d4b0245e6bd6
Manuel <manuelpinedacabeza@gmail.com>
2023-03-14 14:29:45 +0100

    Imported theme utils. Create theme and update typography. Wrapped app with ThemeProvider.

--- src/App.js
+++ src/App.js
@@ -6,16 +6,28 @@ import PastaContent from "./components/Content/PastaContent";
 import About from "./components/Content/About";
 import Location from "./components/Content/Location";
 
+import { createTheme, responsiveFontSizes, ThemeProvider } from "@mui/material/styles";
+
+let theme = createTheme({
+  typography: {
+    fontSize: 10
+  }
+});
+theme = responsiveFontSizes(theme, {factor: 1});
+console.log(theme)
+
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <Content />
-      <PizzaContent />
-      <PastaContent />
-      <About />
-      <Location />
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <Navbar />
+        <Content />
+        <PizzaContent />
+        <PastaContent />
+        <About />
+        <Location />
+      </div>
+    </ThemeProvider>
   );
 }
 
